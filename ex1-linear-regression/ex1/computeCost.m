87f4c6984edbe0c8befc0e16bb49c670a40042af
Young Yu <32204448+yyu2002@users.noreply.github.com>
2018-11-12 12:44:33 -0800

    linear regression

---
+++ ex1-linear-regression/ex1/computeCost.m
@@ -0,0 +1,15 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+J = 0;
+
+h = X*theta;
+squaredErrors = (h - y) .^ 2;
+J = (1/(2*m)) * sum(squaredErrors);
+
+end
